c07141fbcee89fe578ba3fdeb1d14335d1fb839b
Jessica K <jessikim807@gmail.com>
2020-04-11 13:32:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,7 +133,7 @@ def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute,data|
       data.each do |data_item|
-        data_item[points]
+        p data_item[points]
       end
     end
   end
