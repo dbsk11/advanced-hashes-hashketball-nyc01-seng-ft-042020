0b880b2c2e190b5e093da02f74cbf15839475120
Jessica K <jessikim807@gmail.com>
2020-04-11 14:57:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,16 +182,15 @@ end
 def player_stats(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |attribute, data|
-      if attribute == :players
-        data.each do |info|
+      # if attribute == :players
+      #   data.each do |info|
           if player_name == info[:player_name]
             return info
           end
         end
       end
     end
-  end
-end
+
 
 def big_shoe_rebounds
 end
