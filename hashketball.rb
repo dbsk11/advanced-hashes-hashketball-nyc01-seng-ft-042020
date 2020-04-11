56ef2ef1234c0e7d32d3f87d65cbc4f7c2c4bf8e
Jessica K <jessikim807@gmail.com>
2020-04-11 13:45:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,9 @@ end
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute,data|
+      binding.pry
       data.find do |data_item|
+        binding.pry
         if data_item == player_name
         return data_item[:points]
         binding.pry
