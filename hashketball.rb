00e6e57a5c35f235d9d938c11f980cbe2d9fcffd
Jessica K <jessikim807@gmail.com>
2020-04-11 14:56:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,7 +180,6 @@ def player_numbers(team_name)
 end
 
 def player_stats(player_name)
-  player_stats = {}
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       if attribute == :players
